3bdfb963f078d5803b56e0cf0e2ed0c3b97b0738
AlenAbraham02 <alenabraham02@gmail.com>
2019-10-08 13:38:48 +0530

    committ successfull

--- app/src/main/java/com/example/passport/MainActivity.java
+++ app/src/main/java/com/example/passport/MainActivity.java
@@ -12,22 +12,30 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     TextView welcome;
     ImageView frontimage;
-    Button old_user,new_user;
+    Button old_user, new_user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        welcome=findViewById(R.id.textview_welcome);
-        frontimage=findViewById(R.id.frontimg);
-        old_user=findViewById(R.id.btn_olduser);
-        new_user=findViewById(R.id.btn_newuser);
+        welcome = findViewById(R.id.textview_welcome);
+        frontimage = findViewById(R.id.frontimg);
+        old_user = findViewById(R.id.btn_olduser);
+        new_user = findViewById(R.id.btn_newuser);
         new_user.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                Intent intent = new Intent(MainActivity.this, Registrationform.class);
+                startActivity(intent);
             }
         });
+        old_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, login.class);
+                startActivity(intent);
 
+            }
+        });
     }
 }
