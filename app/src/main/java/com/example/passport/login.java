72b26eabba97772cfccec4d18285cc45a8a2abe7
AlenAbraham02 <alenabraham02@gmail.com>
2019-10-08 13:46:00 +0530

    added login page

--- app/src/main/java/com/example/passport/login.java
+++ app/src/main/java/com/example/passport/login.java
@@ -3,12 +3,24 @@ package com.example.passport;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
+    TextView username,password;
+    EditText uname,pswd;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        username=findViewById(R.id.edittext_username);
+        password=findViewById(R.id.textview_psw);
+        uname=findViewById(R.id.edittext_username);
+        pswd=findViewById(R.id.edittext_psw);
+        login=findViewById(R.id.btn_login);
+        
     }
 }
