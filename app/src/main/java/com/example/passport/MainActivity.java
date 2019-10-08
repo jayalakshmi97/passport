3eeeec978fb6947d1e2dbfd27bebb6a6d1a52cec
AlenAbraham02 <alenabraham02@gmail.com>
2019-10-08 12:50:40 +0530

    committ successfull

--- app/src/main/java/com/example/passport/MainActivity.java
+++ app/src/main/java/com/example/passport/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.passport;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView welcome;
+    ImageView frontimage;
+    Button old_user,new_user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        welcome=findViewById(R.id.textview_welcome);
+        frontimage=findViewById(R.id.frontimg);
+        old_user=findViewById(R.id.btn_olduser);
+        new_user=findViewById(R.id.btn_newuser);
+        new_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
     }
 }
