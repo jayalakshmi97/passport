05b4a2e1a02810e77c8ee7446d8f2caeac405d67
AlenAbraham02 <alenabraham02@gmail.com>
2019-10-09 12:16:23 +0530

    modified login page

--- app/src/main/java/com/example/passport/login.java
+++ app/src/main/java/com/example/passport/login.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class login extends AppCompatActivity {
     TextView username,password;
@@ -21,6 +22,25 @@ public class login extends AppCompatActivity {
         uname=findViewById(R.id.edittext_username);
         pswd=findViewById(R.id.edittext_psw);
         login=findViewById(R.id.btn_login);
+        //to check whether password or username field is empty
+        if (uname.getText().toString().isEmpty()||pswd.getText().toString().isEmpty())
+        {
+            Toast.makeText(login.this,"Empty field exist",Toast.LENGTH_LONG).show();
+            if (uname.getText().toString().isEmpty())
+            {
+                uname.setError("Type your username");
+            }
+            else
+            {
+                pswd.setError("Type your password");
+            }
+        }
+        else
+        {
+            Toast.makeText(login.this,"Login successfull",Toast.LENGTH_LONG).show();
+            uname.setText("null");
+            pswd.setText("null");
+        }
 
     }
 }
