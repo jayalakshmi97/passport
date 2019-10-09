352653460a4726b8147ebbe0ef82963875df69ed
AlenAbraham02 <alenabraham02@gmail.com>
2019-10-09 13:20:40 +0530

    login page completed

--- app/src/main/java/com/example/passport/login.java
+++ app/src/main/java/com/example/passport/login.java
@@ -3,6 +3,8 @@ package com.example.passport;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -48,6 +50,12 @@ public class login<stringRequest> extends AppCompatActivity {
             Toast.makeText(login.this, "Login successfull", Toast.LENGTH_LONG).show();
             uname.setText("null");
             pswd.setText("null");
+        }
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
             StringRequest stringRequest = new StringRequest(Request.Method.POST,"https://gressorial-parts.000webhostapp.com/login.php",
                     new Response.Listener<String>() {
                         @Override
@@ -89,14 +97,14 @@ params.put("pswd",pswd.getText().toString());
             };
 
 //Adding the string request to the queue
-            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            RequestQueue requestQueue = Volley.newRequestQueue(login.this);
             requestQueue.add(stringRequest);
-
+            }
+        });
 
         }
     }
 
-}
 
 
 
