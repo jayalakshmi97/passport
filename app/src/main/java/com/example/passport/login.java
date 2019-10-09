cb61603301e97404c1fbb6fb0596b500841bca6b
AlenAbraham02 <alenabraham02@gmail.com>
2019-10-09 13:08:41 +0530

    login page completed except url

--- app/src/main/java/com/example/passport/login.java
+++ app/src/main/java/com/example/passport/login.java
@@ -16,6 +16,10 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -44,7 +48,50 @@ public class login<stringRequest> extends AppCompatActivity {
             Toast.makeText(login.this, "Login successfull", Toast.LENGTH_LONG).show();
             uname.setText("null");
             pswd.setText("null");
-            // Instantiate the RequestQueue.
+            StringRequest stringRequest = new StringRequest(Request.Method.POST,"anoopsuvarnan1.000webhostapp.com/fetchlocationapi.php",
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+//If we are getting success from server
+
+
+                            /*try {
+                                JSONArray jsonArray=new JSONArray(response);
+                                for(int i=0;i<jsonArray.length();i++){
+                                    JSONObject json_obj = jsonArray.getJSONObject(i);
+                                    latitude = json_obj.getString("latitude");
+                                    longitude=json_obj.getString("longitude");
+                                    Toast.makeText(MapsActivity.this,latitude,Toast.LENGTH_SHORT).show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }*/
+
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+//You can handle error here if you want
+                        }
+
+                    }){
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String,String> params = new HashMap<>();
+//Adding parameters to request
+params.put("username",uname.getText().toString());
+params.put("pswd",pswd.getText().toString());
+
+//returning parameter
+                    return params;
+                }
+            };
+
+//Adding the string request to the queue
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            requestQueue.add(stringRequest);
+
 
         }
     }
